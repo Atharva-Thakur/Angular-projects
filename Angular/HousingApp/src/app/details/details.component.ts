ea518a9241aa0a764b290ebe6f54d7f4ecf2248d
Atharva Thakur <atharvathakur369@gmail.com>
2024-06-15 02:45:37 +0530

    Completed Housing App

--- Angular/HousingApp/src/app/details/details.component.ts
+++ Angular/HousingApp/src/app/details/details.component.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { Housinglocation } from '../housinglocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   template: `
   <article>
     <img class="listing-photo" [src]="housingLocation?.photo"
@@ -19,9 +20,29 @@ import { Housinglocation } from '../housinglocation';
       <h2 class="section-heading">About this housing location</h2>
       <ul>
         <li>Units available: {{housingLocation?.availableUnits}}</li>
-        <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
-        <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
+        <li *ngIf="housingLocation?.wifi;else noWifiBlock">Does this location have wifi: Yes</li>
+        <ng-template #noWifiBlock>
+          <li>Does this location have wifi: No</li>
+        </ng-template>
+        <li *ngIf="housingLocation?.laundry;else noLaundryBlock">Does this location have laundry: Yes</li>
+        <ng-template #noLaundryBlock>
+          <li>Does this location have laundry: No</li>
+        </ng-template>
       </ul>
+    </section>
+    <section class="listing-apply">
+      <h2 class="section-heading">Apply now to live here</h2>
+      <form [formGroup]="applyForm" (submit)="submitApplication()">
+        <label for="first-name">First Name</label>
+        <input id="first-name" type="text" formControlName="firstName">
+
+        <label for="last-name">Last Name</label>
+        <input id="last-name" type="text" formControlName="lastName">
+
+        <label for="email">Email</label>
+        <input id="email" type="email" formControlName="email">
+        <button type="submit" class="primary">Apply now</button>
+      </form>
     </section>
   </article>
 `,
@@ -33,9 +54,25 @@ export class DetailsComponent {
   housingService = inject(HousingService);
   housingLocation: Housinglocation | undefined;
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    this.housingService.getHousingLocationById(housingLocationId).then((housingLocation:Housinglocation | undefined)=>{
+      this.housingLocation = housingLocation
+    })
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 
 }
--- Angular/HousingApp/src/app/home/home.component.ts
+++ Angular/HousingApp/src/app/home/home.component.ts
@@ -9,26 +9,39 @@ import { Housinglocation } from '../housinglocation';
   imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
-    <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
-    </form>
-  </section>
-  <section class="results">
-  <app-housing-location *ngFor="let housinglocation of housingLocationList " [housinglocation]="housinglocation"></app-housing-location>
-  <!-- <app-housing-location [housinglocation]="housingLocationList[0]"></app-housing-location>
-<app-housing-location [housinglocation]="housingLocationList[1]"></app-housing-location>
-<app-housing-location [housinglocation]="housingLocationList[2]"></app-housing-location> -->
-
-  </section>
+      <form>
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location
+        *ngFor="let housinglocation of filteredLocationList"
+        [housinglocation]="housinglocation">
+      </app-housing-location>
+    </section>
   `,
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   housingLocationList: Housinglocation[] = [];
   housingService: HousingService = inject(HousingService);
-
+  filteredLocationList: Housinglocation[] = [];
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService.getAllHousingLocations().then((housingLocationList:Housinglocation[])=>{
+      this.housingLocationList = housingLocationList
+      this.filteredLocationList = housingLocationList;
+    })
+    
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+  
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
